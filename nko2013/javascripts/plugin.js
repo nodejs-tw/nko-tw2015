d348f5067f14fa0ff4992cebcd363b3f97d43ffa
webber_wu <webber_wu@hiiir.com>
2015-10-10 18:27:16 +0800

    新增舊的資料過來

---
+++ nko2013/javascripts/plugin.js
@@ -0,0 +1,28 @@
+// fB
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=470728142953792";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+//G+
+window.___gcfg = {lang: 'zh-TW'};
+(function() {
+ var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+ po.src = 'https://apis.google.com/js/plusone.js';
+ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+ })();
+//Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+// GA
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-35142334-1']);
+_gaq.push(['_setDomainName', 'nodejs.tw']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+window.jfAsyncInit=function(){ctb.main({'appId':'adfc0d2cC1SuRHnM2HFMu-aG94RSHQFQEt6E8qzYjDhyCcq-xWIY2OSRaNGkEgLNAJRaAnUtuaiNvZMfhF1BZe2G5iiWOMfxUqfbssuVLvoP5NDQl5VSqRrqgK8ALjGSdQ75HKKTCTfX_2_tvCcPMM-rEKJS8L-DOCGXr-TA4xjD-rO0-9A='});};(function(){var jf=document.createElement('script');jf.type='text/javascript';jf.async=true;jf.src='http://ds.justfont.com/core/js/v1.0/adfc0d2cC1SuRHnM2HFMu-aG94RSHQFQEt6E8qzYjDhyCcq-xWIY2OSRaNGkEgLNAJRaAnUtuaiNvZMfhF1BZe2G5iiWOMfxUqfbssuVLvoP5NDQl5VSqRrqgK8ALjGSdQ75HKKTCTfX_2_tvCcPMM-rEKJS8L-DOCGXr-TA4xjD-rO0-9A=.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(jf,s);})();
